604250d26c2bbedfe70939499555a28e2d37375b
Khusainov Ruslan <khusainovheckfy@gmail.com>
2024-12-17 09:45:20 +0700

    Init

---
+++ RestaurantChain/RestaurantChain.DomainServices/DomainServicesRegistration.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace RestaurantChain.DomainServices
+{
+    public static class DomainServicesRegistration
+    {
+        public static void UseDomainServices(this IServiceCollection services)
+        {
+        }
+    }
+}
